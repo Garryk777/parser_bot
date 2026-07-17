53411adc28732bf93e5eda00ad1515a30b251df3
Garryk777 <Dorofeevigor2@gmail.com>
2026-07-18 01:24:30 +0300

    Add files via upload

---
+++ chats.py
@@ -0,0 +1,9 @@
+# chats.py
+# Список чатов для мониторинга (username без @)
+CHATS = [
+    "thailand_hotels_travelask",
+    "phuket_rent",
+    "bangkok_apartments",
+    "chiang_mai_exchange",
+    # Добавляй новые чаты сюда
+]
