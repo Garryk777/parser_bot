c4c71390117422d8177e0c0f5bca5df8d58d308d
Garryk777 <Dorofeevigor2@gmail.com>
2026-07-18 01:16:11 +0300

    Delete parser_bot-main directory

--- parser_bot-main/chats.py
+++
@@ -1,9 +0,0 @@
-# chats.py
-# Список чатов для мониторинга (username без @)
-CHATS = [
-    "thailand_hotels_travelask",
-    "phuket_rent",
-    "bangkok_apartments",
-    "chiang_mai_exchange",
-    # Добавляй новые чаты сюда
-]
